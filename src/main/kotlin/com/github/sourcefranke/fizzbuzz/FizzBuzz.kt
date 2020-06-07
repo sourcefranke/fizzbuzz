b1a61df1e8e5116f6445cc358e98215ca7116555
Lukas Tietenberg <sourcefranke@users.noreply.github.com>
2020-06-07 21:38:22 +0200

    Introduced function parameters (#5)
    Co-authored-by: sourcefranke <lukas.tietenberg@outlook.de>

--- src/main/kotlin/com/github/sourcefranke/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/com/github/sourcefranke/fizzbuzz/FizzBuzz.kt
@@ -1,13 +1,36 @@
 package com.github.sourcefranke.fizzbuzz
 
-fun fizzBuzzNumber(wordsMap: Map<Int, String>, number: Int) =
+fun fizzBuzzNumber (
+        number: Int,
+        wordsMap: Map<Int, (Int) -> String> = mapOf(3 to ::fizz, 5 to ::buzz),
+        default: (Int) -> String = ::numberToString
+) =
         wordsMap.keys
                 .filter { modulo -> number % modulo == 0 }
                 .map { modulo -> wordsMap[modulo] }
+                .map { u -> u?.invoke(number) }
                 .filter { it!!.isNotEmpty() }
                 .joinToString ( separator = "" )
-                .ifEmpty { number.toString() }
+                .ifEmpty { default(number) }
+
+fun fizzBuzzList (
+        numberList: List<Int>,
+        wordsMap: Map<Int, (Int) -> String> = mapOf(3 to ::fizz, 5 to ::buzz),
+        default: (Int) -> String = ::numberToString
+) =
+        numberList.map { number -> fizzBuzzNumber(number, wordsMap, default) }
 
 
-fun fizzBuzzList(wordsMap: Map<Int, String>, numberList: List<Int>) =
-        numberList.map { number -> fizzBuzzNumber(wordsMap, number) }
+// Helper functions for replacing numbers by text
+
+fun numberToString (number: Int) = number.toString()
+@Suppress("UNUSED_PARAMETER")
+fun fizz (number: Int) = "Fizz"
+@Suppress("UNUSED_PARAMETER")
+fun buzz (number: Int) = "Buzz"
+
+fun repeatNumber (number: Int): String {
+    var result = ""
+    repeat(number) { result += number }
+    return result
+}
--- src/test/kotlin/com/github/sourcefranke/fizzbuzz/FizzBuzzTest.kt
+++ src/test/kotlin/com/github/sourcefranke/fizzbuzz/FizzBuzzTest.kt
@@ -1,15 +1,12 @@
 package com.github.sourcefranke.fizzbuzz
 
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
 
 class FizzBuzzTest {
 
     @Test
-    fun testFizzBuzzNumber() {
-        val wordsMap = mapOf(3 to "Fizz", 5 to "Buzz")
-
+    fun `Test fizzBuzzNumber` () {
         listOf(
                 Pair(1, "1"),
                 Pair(2, "2"),
@@ -29,17 +26,52 @@ class FizzBuzzTest {
                 Pair(16, "16")
         )
         .forEach {
-            (input, result) -> assertThat(fizzBuzzNumber(wordsMap, input)).isEqualTo(result)
+            (input, result) -> assertThat(fizzBuzzNumber(input)).isEqualTo(result)
         }
     }
 
     @Test
-    fun testFizzBuzzList() {
+    fun `Test fizzBuzzList` () {
         val inputList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
-        val wordsMap = mapOf(3 to "Fizz", 5 to "Buzz")
 
-        assertThat(fizzBuzzList(wordsMap, inputList))
+        assertThat(fizzBuzzList(inputList))
                 .hasSize(16)
                 .containsExactly("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16")
+    }
+
+    @Test
+    fun `Test repeatNumber` () {
+        listOf(
+                Pair(1, "1"),
+                Pair(2, "22"),
+                Pair(3, "333"),
+                Pair(4, "4444"),
+                Pair(5, "55555"),
+                Pair(6, "666666"),
+                Pair(7, "7777777"),
+                Pair(8, "88888888"),
+                Pair(9, "999999999"),
+                Pair(10, "10101010101010101010")
+        )
+        .forEach {
+            (input, result) -> assertThat(repeatNumber(input)).isEqualTo(result)
+        }
+    }
+
+    @Test
+    fun `Test fizzBuzzList with repeatNumber as Default` () {
+        val inputList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+        assertThat(fizzBuzzList(inputList, default = ::repeatNumber))
+                .containsExactly("1", "22", "Fizz", "4444", "Buzz", "Fizz", "7777777", "88888888", "Fizz")
+    }
+
+    @Test
+    fun `Test fizzBuzzList with repeatNumber for %3` () {
+        val inputList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+        val wordsMap = mapOf(3 to ::repeatNumber, 5 to ::buzz)
+
+        assertThat(fizzBuzzList(inputList, wordsMap))
+                .containsExactly("1", "2", "333", "4", "Buzz", "666666", "7", "8", "999999999")
     }
 }
